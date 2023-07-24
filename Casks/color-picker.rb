88ed3629777955f773997d42b9a3cd933bfd8d62
Akinori MUSHA <knu@idaemons.org>
2023-07-24 23:15:14 +0900

    Add "Color Picker" (Non-App Store version)

---
+++ Casks/color-picker.rb
@@ -0,0 +1,27 @@
+cask "color-picker" do
+  version "1.13.0,1679983870"
+  sha256 "15db2bea2ba350d1b4e62598e0b36f67f06cc4976cf605e6de1a14ea251f4e06"
+
+  url "https://dsc.cloud/sindresorhus/Color-Picker-#{version.csv.first}-#{version.csv.second}.zip",
+      verified: "dsc.cloud/sindresorhus/"
+  name "Color Picker"
+  desc "The familiar color picker supercharged"
+  homepage "https://sindresorhus.com/system-color-picker"
+
+  livecheck do
+    url "https://github.com/sindresorhus/System-Color-Picker"
+    regex(/href.*?Color-Picker-(\d+(?:\.\d+)+)-(\w+)\.zip"/i)
+    strategy :page_match do |page, regex|
+      page.scan(regex).map { |match| "#{match[0]},#{match[1]}" }
+    end
+  end
+
+  depends_on macos: ">= :ventura"
+
+  app "Color Picker.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/com.sindresorhus.Color-Picker",
+    "~/Library/Containers/com.sindresorhus.Color-Picker",
+  ]
+end
