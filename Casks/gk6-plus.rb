fc05b6638c70c21bb503f2c98c3e4197fd695ecb
Akinori MUSHA <knu@idaemons.org>
2023-10-20 03:54:13 +0900

    Update the download URL; no longer accessible via https

--- Casks/gk6-plus.rb
+++ Casks/gk6-plus.rb
@@ -2,7 +2,7 @@ cask "gk6-plus" do
   version "1.0.0.1"
   sha256 "c896df76e567ba842083042e4ed14d6b14efc7f5ba8988247d4cbc9b2e609aee"
 
-  url "https://cloudmouse.oss-cn-beijing.aliyuncs.com/AppData/90/GK6x%20Mac%20App-#{version}.dmg",
+  url "http://cloudmouse.oss-cn-beijing.aliyuncs.com/AppData/90/GK6X%20Mac%20App-#{version}.dmg",
       verified: "cloudmouse.oss-cn-beijing.aliyuncs.com/AppData/"
   name "GK6+"
   desc "Keyboard utility for GK6 series PCB/Keyboards"
