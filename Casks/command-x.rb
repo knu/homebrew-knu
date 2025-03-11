f9e424627d93efc2b853b41541bf5c735af6f53e
Akinori MUSHA <knu@idaemons.org>
2025-03-11 15:23:21 +0900

    Use dl.dropboxusercontent.com to avoid redirection

--- Casks/command-x.rb
+++ Casks/command-x.rb
@@ -2,8 +2,8 @@ cask "command-x" do
   version "1.4.0-1705659953,3zg73o7fs2rlpypaud9pj,x9gpppflpv4ve2t2qm1yg5ht9"
   sha256 "8112631130d07332ce4fd403eeb248f060cbfa014641fa7dbc318fb3e56e198c"
 
-  url "https://www.dropbox.com/scl/fi/#{version.csv.second}/Command-X-#{version.major_minor_patch}.zip?rlkey=#{version.csv.third}&raw=1",
-      verified: "dropbox.com/scl/fi/"
+  url "https://dl.dropboxusercontent.com/s/#{version.csv.second}/Command-X-#{version.major_minor_patch}.zip?rlkey=#{version.csv.third}",
+      verified: "dl.dropboxusercontent.com/s/"
   name "Command X"
   desc "Cut and paste files in Finder"
   homepage "https://sindresorhus.com/command-x"
